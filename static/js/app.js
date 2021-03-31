b3be8d73a28384a7df778caefdadd7057eec3232
Pamela Prybylski <mori@inari.garbagefile.net>
2021-03-31 18:06:07 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,36 @@
-// function getPlots(id) {
-      // TODO: 1. Use the D3 library to read in `samples.json`.
-      d3.json('../../data/samples.json').then(sampledata => {
-            console.log(sampledata)
-      })
-
-      // TODO: 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-      // * Use `sample_values` as the values for the bar chart.
-      // * Use `otu_ids` as the labels for the bar chart.
-      // * Use `otu_labels` as the hovertext for the chart.
-
-      var ids = sampledata.samples[0].out_ids.reverse().slice(0,10);
-      console.log(ids)
+// TODO: Wrap both plots code in a function called getPlots(id)
 
 
+// TODO: Use the D3 library to read in `samples.json`.
+d3.json('../../data/samples.json').then(sampledata => {
+      console.log(sampledata)
+})
 
-// TODO: 3. Create a bubble chart that displays each sample.
-      // * Use `otu_ids` for the x values.
-      // * Use `sample_values` for the y values.
-      // * Use `sample_values` for the marker size.
-      // * Use `otu_ids` for the marker colors.
-      // * Use `otu_labels` for the text values.
+// TODO: Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// * Use `sample_values` as the values for the bar chart.
+// * Use `otu_ids` as the labels for the bar chart.
+// * Use `otu_labels` as the hovertext for the chart.
 
-// }
+var ids = sampledata.samples[0].out_ids.reverse().slice(0,10);
+console.log(ids)
 
-// TODO: 4. Display the sample metadata, i.e., an individual's demographic information.
 
-// TODO: 5. Display each key-value pair from the metadata JSON object somewhere on the page.
 
-// TODO: 6. Update all of the plots any time that a new sample is selected.
+// TODO: Create a bubble chart that displays each sample.
+// * Use `otu_ids` for the x values.
+// * Use `sample_values` for the y values.
+// * Use `sample_values` for the marker size.
+// * Use `otu_ids` for the marker colors.
+// * Use `otu_labels` for the text values.
 
-// TODO: * Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
 
-// TODO: * Ensure your repository has regular commits (i.e. 20+ commits) and a thorough README.md file
+
+// TODO: Display the sample metadata, i.e., an individual's demographic information.
+
+// TODO: Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// TODO: Update all of the plots any time that a new sample is selected.
+
+// TODO: Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
+
+// TODO: Ensure your repository has regular commits (i.e. 20+ commits) and a thorough README.md file
