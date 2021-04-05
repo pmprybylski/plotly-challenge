5ac12140d610d0edea4d0465e068e030609d13bf
Pamela Prybylski <mori@inari.garbagefile.net>
2021-04-05 17:09:52 -0400

    code updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,19 @@
 // Set up function to generate plots
 function getPlots(id) {
-// Use the D3 library to read in `samples.json`.
+      // Use the D3 library to read in `samples.json`.
       d3.json('../../data/samples.json').then(sampledata => {
             console.log(sampledata)
 
             // Set variables, pulling only the top 10 by OTU
-            var ids = sampledata.samples.filter(sample => sample.id == id)[0].otu_ids.slice(0,10).reverse()
-            console.log(ids);
-            var values = sampledata.samples.filter(sample => sample.id == id)[0].sample_values.slice(0,10).reverse()
+            var otu_ids = sampledata.samples[0].otu_ids.slice(0,10).reverse()
+            console.log(otu_ids);
+            var values = sampledata.samples[0].sample_values.slice(0,10).reverse()
             console.log(values)
-            var labels = sampledata.samples.filter(sample => sample.id == id)[0].otu_labels.slice(0,10).reverse() 
-            console.log(labels)
+            var otu_labels = sampledata.samples[0].otu_labels.slice(0,10).reverse() 
+            console.log(otu_labels)
 
             // Convert ID data to display properly
-            var otuIds = ids.map(id => 'OTU ' + id)
+            var otuIds = otu_ids.map(otu_id => 'OTU ' + otu_id)
 
             // * Use `sample_values` as the values for the bar chart.
             // * Use `otu_ids` as the labels for the bar chart.
@@ -21,7 +21,7 @@ function getPlots(id) {
             var trace_bar = {
                   x: values,
                   y: otuIds,
-                  text: labels,
+                  text: otu_labels,
                   type: 'bar',
                   marker: { color: '#B20A1C'},
                   orientation: 'h'
@@ -39,9 +39,9 @@ function getPlots(id) {
 
             // Create a bubble chart that displays each sample.
             var trace_bubble = {
-                  x: sampledata.samples.filter(sample => sample.id == id)[0].otu_ids,
-                  y: sampledata.samples.filter(sample => sample.id == id)[0].sample_values,
-                  text: sampledata.samples.filter(sample => sample.id == id)[0].otu_labels,
+                  x: sampledata.samples[0].otu_ids,
+                  y: sampledata.samples[0].sample_values,
+                  text: sampledata.samples[0].otu_labels,
                   mode: 'markers',
                   marker: {
                         color: sampledata.samples[0].otu_ids,
@@ -61,14 +61,58 @@ function getPlots(id) {
       });
 }
 
-// TODO: Display the sample metadata, i.e., an individual's demographic information.
+// Display the sample metadata, i.e., an individual's demographic information.
+function getDemo(id) {
+      d3.json('../../data/samples.json').then((sampledata) => {
+            var metaID = sampledata.metadata[0].id
+            var ethnicity = sampledata.metadata[0].ethnicity
+            var gender = sampledata.metadata[0].gender
+            var age = sampledata.metadata[0].age
+            var location = sampledata.metadata[0].location
+            var bbtype = sampledata.metadata[0].bbtype
+            var wfreq = sampledata.metadata[0].wfreq
+            
+            function demoTable(metaID, ethnicity, gender, age, location, bbtype, wfreq) {
+                  var table = d3.select('#summary-table')
+                  var tbody = table.select('tbody')
+                  var trow
 
+                  trow = tbody.append('td')
+                  trow.append('td').text(metaID)
+                  trow.append('td').text(ethnicity)
+                  trow.append('td').text(gender)
+                  trow.append('td').text(age)
+                  trow.append('td').text(location)
+                  trow.append('td').text(bbtype)
+                  trow.append('td').text(wfreq)
+            }
+      })
+}
 
+// Create the function for the change event
+function optionChanged(id) {
+      getPlots(id)
+      getDemo(id)
+}
 
-// TODO: Display each key-value pair from the metadata JSON object somewhere on the page.
+// Create function for initial rendering
+function init() {
+      // Select dropdown menu
+      var dropdown = d3.select('#selDataset')
 
-// TODO: Update all of the plots any time that a new sample is selected.
+      // Read the data
+      d3.json('../../data/samples.json').then(sampledata => {
+            console.log(sampledata)
 
-// TODO: Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
+            // Get the id data to the dropdown menu
+            sampledata.names.forEach(function(name) {
+                  dropdown.append('option').text(name).property('value')
+            })
 
-// TODO: Ensure your repository has regular commits (i.e. 20+ commits) and a thorough README.md file
+            // Call the functions to display data and plots to page
+            getPlots(sampledata.names[0])
+            getDemo(sampledata.names[0])
+      })
+}
+
+init()
